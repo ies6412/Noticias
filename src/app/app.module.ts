809a26b6104aba46f21187c8defb9d36df9b5b79
ies6412 <63573807+ies6412@users.noreply.github.com>
2020-09-10 22:43:56 -0500

    implemtaci de Inbrows native propio del telefono

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     StatusBar,
+    InAppBrowser,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../interfaces/noticia-interface';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-noticia',
@@ -10,8 +11,14 @@ export class NoticiaComponent implements OnInit {
 
   @Input() noticia: Article;
   @Input() i: number;
-  constructor() { }
+  constructor( private urlnoticia: InAppBrowser) { }
 
   ngOnInit() {}
+  AbrirNoticia(){
+
+    //console.log('noticia-->', this.noticia.url);
+    const browser = this.urlnoticia.create(this.noticia.url, '_system');
+
+  }
 
 }
